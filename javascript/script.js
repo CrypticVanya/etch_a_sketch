98244c5bd4f1c0668275103e6b521ece36ec7fcd
Vanya <crypticvanya@gmail.com>
2022-05-11 19:19:38 -0400

    add a button that deploys a prompt for grid size

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,38 @@
 const container = document.getElementById("container")
+const btn = document.querySelector("#grid-btn")
+
+btn.addEventListener('click', () => {
+    let answer = Number(prompt("How many grids?"))
+    if (answer < 0) {
+        alert("ERROR")  
+    } else if (answer <= 100) {
+        container.innerHTML = ''
+        makeRows(answer, answer)
+        hoverEffect() 
+    } else {
+        alert("ERROR")
+    }
+})
+
+
 
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows)
     container.style.setProperty('--grid-cols', cols)
     for (let i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div")
-        cell.innerText = (i + 1)
+        cell.innerText = ''
         container.appendChild(cell).className = "grid-item"
     }
 }
 
 makeRows(16,16)
-
 // loop to create the grid
 
+
+
+
+function hoverEffect() {
 const gridItem = document.querySelectorAll(".grid-item")
 
 gridItem.forEach((item) => {
@@ -25,4 +44,11 @@ gridItem.forEach((item) => {
         }, 750)
     }, false)
 })
+
+}
+
+hoverEffect()
+
+//create hover effect
+
 
