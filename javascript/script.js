d998528bda0a16b9dba7752a31911da4de43f2b0
Vanya <crypticvanya@gmail.com>
2022-06-12 21:20:26 -0400

    update UI color

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,11 @@
+
+//Grab divs from the DOM where the cell will be placed along with the button to activate it
+
 const container = document.getElementById("container")
 const btn = document.querySelector("#grid-btn")
+
+
+// function that executes when the button is clicked, allows for the manipulation of the grid
 
 btn.addEventListener('click', () => {
     let answer = Number(prompt("How many grids?"))
@@ -15,6 +21,7 @@ btn.addEventListener('click', () => {
 })
 
 
+// loop that creates the cell in the dom
 
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows)
@@ -27,28 +34,23 @@ function makeRows(rows, cols) {
 }
 
 makeRows(16,16)
-// loop to create the grid
 
 
-
+//create hover effect when passing over cells
 
 function hoverEffect() {
-const gridItem = document.querySelectorAll(".grid-item")
+    const gridItem = document.querySelectorAll(".grid-item")
 
-gridItem.forEach((item) => {
-    item.addEventListener('mouseenter', (e) => {
-        e.target.style.background = 'purple'
+    gridItem.forEach((item) => {
+        item.addEventListener('mouseenter', (e) => {
+            e.target.style.background = 'rgba(173, 0, 135, 0.25)'
 
-        setTimeout(function() {
-            e.target.style.background = ""
-        }, 750)
-    }, false)
+            setTimeout(function() {
+                e.target.style.background = ""
+            }, 1000)
+    })
 })
 
 }
 
 hoverEffect()
-
-//create hover effect
-
-
